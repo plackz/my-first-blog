8100164c184be42e229158156355a8812052dbb6
plackz <pzaffina@slb.com>
2017-02-03 07:38:18 -0600

    minor tweks from office

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,12 @@ from .models import Post
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts}) 
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
